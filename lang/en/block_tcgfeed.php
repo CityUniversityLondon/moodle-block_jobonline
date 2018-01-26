22b7d85eca7673276ed35c494250c5e307868255
Thomas Worthington <thomas@technouveau.co.uk>
2018-01-26 11:21:04 +0000

    Initial skeleton version

---
+++ lang/en/block_tcgfeed.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Version details
+ *
+ * @package    tcgfeed
+ * @copyright  2018 onwards University of London
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname']='TCG Opportunities Block';
+$string['pluginnameplural'] = 'TCG Opportunities Blocks';
