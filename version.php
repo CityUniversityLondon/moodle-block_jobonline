c327a0e221c2ce98c29109a6703b6734d4b95043
Thomas Worthington <thomas@technouveau.co.uk>
2018-03-12 12:29:24 +0000

    Further renaming work.

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018030602;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2018031201;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016112900;        // Requires this Moodle version
 $plugin->component = 'block_jobonline'; // Full name of the plugin (used for diagnostics)
