6aeb24df7852f07f63a4ee7523a0a1d1f5ac78ec
Thomas Worthington <thomas@technouveau.co.uk>
2019-08-12 12:49:58 +0100

    Bumped version.

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018040602;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019081201;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016112900;        // Requires this Moodle version
 $plugin->component = 'block_jobonline'; // Full name of the plugin (used for diagnostics)
